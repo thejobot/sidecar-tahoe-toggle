5e3f476f0e2e0dfe3d205fc68f2e272d1b15cef8
thejobot <34147824+thejobot@users.noreply.github.com>
2026-02-19 18:12:13 -0600

    install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-
 echo "================================"
 echo "  Sidecar Tahoe Toggle Installer"
 echo "================================"
 echo ""
 read -p "Enter your iPad's name (press Enter if it's just 'iPad'): " ipad_name
 ipad_name=${ipad_name:-iPad}
-
 echo ""
 echo "Setting up sidecar_toggle.swift for '$ipad_name'..."
-
 curl -s https://raw.githubusercontent.com/thejobot/sidecar-tahoe-toggle/main/sidecar_toggle.swift \
-    | sed "s/IPAD_NAME_HERE/$ipad_name/g" \
+    | sed "s|IPAD_NAME_HERE|${ipad_name}|g" \
     > ~/sidecar_toggle.swift
-
 echo "✓ Done! Testing now..."
 swift ~/sidecar_toggle.swift
 echo ""
